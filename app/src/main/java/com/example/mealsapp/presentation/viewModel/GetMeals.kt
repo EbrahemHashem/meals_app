4f61842c2a65319d5ba2ab4abcb1868f618493a4
Ebrahem <ebrahemhashem72@gmail.com>
2024-10-13 15:33:40 +0300

    create view model

--- app/src/main/java/com/example/mealsapp/presentation/viewModel/GetMeals.kt
+++ app/src/main/java/com/example/mealsapp/presentation/viewModel/GetMeals.kt
@@ -1,6 +1,30 @@
 package com.example.mealsapp.presentation.viewModel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mealsapp.data.model.CategoryResponse
+import com.example.mealsapp.data.network.RetrofitInstance
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class GetMeals:ViewModel() {
+class GetMealsViewModel:ViewModel() {
+    val apiService = RetrofitInstance.apiService
+    private val _CategoryResponse = MutableStateFlow<CategoryResponse?>(null)
+    val categoryResponse: StateFlow<CategoryResponse?> get() = _CategoryResponse
+
+    fun category() {
+        viewModelScope.launch { 
+            try {
+                _CategoryResponse.value = apiService.getMealsData().body()
+
+            }catch (e:Exception){
+                Log.e("MealsViewModel",e.message.toString())
+            }
+
+
+
+        }
+    }
 }
